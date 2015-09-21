1195b6ca7c9111a88610f106e6d1875ee99adeac
rune <ruk@gmail.com>
2015-09-21 14:42:58 +0200

    Added message url

--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('store.jade');
+  res.render('store.jade',{ messageURL: process.env.MESSAGE_URL});
+});
+
+router.route('/message').post(function(req, res, next) {
+  console.log('Receiving message' + req.body.message);
+  res.send('Message was ' + req.body.message);
 });
 
 module.exports = router;
