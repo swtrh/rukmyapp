9d5eeca359a34688736f5698bb673b6fb1e0ba39
rune <ruk@gmail.com>
2015-09-21 13:46:48 +0200

    Added store

--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Greetings, welcome to Express', heading: 'hello' });
+  res.render('store.jade');
 });
 
 module.exports = router;
